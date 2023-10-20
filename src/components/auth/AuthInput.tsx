7507ab003ab93b37d4f4458fbc9b0431fa032598
Jonathan Cortoppassi <jonathan-516@hotmail.com>
2023-10-20 15:40:43 -0300

    update

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -2,24 +2,28 @@ interface AuthIputProps {
     label: string
     valor: any
     obrigatorio?: boolean
-    tipo: 'text' | 'email' | 'password'
+    naoRenderizarQuando?: boolean
+    tipo?: 'text' | 'email' | 'password'
     valorMudou : (novoValor: any) => void
 }
 
 import React from 'react'
 
 export default function AuthInput(props: AuthIputProps) {
-  return (
-    <div>
-        <label className='flex flex-col'>{props.label}</label>
-            <input 
-                type={props.tipo ?? 'text'}
-                value={props.valor}
-                onChange={e => props.valorMudou?.(e.target.value)}
-                required={props.obrigatorio}
-            />
-            
-       
+  return props.naoRenderizarQuando ? null : (
+    <div className='flex flex-col mt-4'>
+        <label>{props.label}</label>
+        <input 
+            type={props.tipo ?? 'text'}
+            value={props.valor}
+            onChange={e => props.valorMudou?.(e.target.value)}
+            required={props.obrigatorio}
+            className={`
+              px-4 py-3 rounded-lg bg-gray-200 mt-2
+              border focus:border-blue-500 focus:bg-white
+              focus:outline-none
+            `}
+        />
     </div>
   )
 }
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -5,7 +5,7 @@ export default function notificacoes() {
   return (
     <Layout titulo="Notificações"
       subtitulo="Aqui você irá gerenciar as suas notificações">
-        <h1></h1>
+        <h1>Notificações</h1>
     </Layout>
   )
 }
