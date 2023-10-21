95891d02fa655941493245d5a14eda186430a5d8
Jonathan Cortoppassi <jonathan-516@hotmail.com>
2023-10-21 07:51:58 -0300

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-if(firebase.app.length) {
+if(!firebase.app.length) {
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLICFIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLICFIREBASE_AUTH_DOMAIN,
--- src/model/Usuario.ts
+++ src/model/Usuario.ts
@@ -1,4 +1,4 @@
-export default interface {
+export default interface Usuario {
     uid: string
     email: string
     nome: string
