029f7cde356c938b88303613f01ba40ff59069ca
Jonathan <jonathan-516@hotmail.com>
2024-01-26 19:05:23 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Layout from '../components/template/Layout'
+import Card from '../components/Card'
 export default function Home() {
   return (
     <Layout titulo="Página Inicial" subtitulo="Estamos construindo um template Admin!">
       <h3>Conteúdo!!!!</h3>
+      <Card/>
+      <Card/>
+      <Card/>
     </Layout>
   )
 }
